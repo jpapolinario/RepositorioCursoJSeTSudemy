58e922c52ba99544a686fbcc37fea58824ec401b
jpapolinario <joaopedrofariaknu@gmail.com>
2026-07-17 17:11:19 -0300

    como pegar uma fonte no google fonts

--- Secao3/comandos.js
+++ Secao3/comandos.js
@@ -2,6 +2,8 @@
 google fonts
 procure a fonte que quer
 entra nela
-seleciona a fonte 
+vai no código dela
+@import para importar no seu css
+e pega tudo dentro do style que for do "Embed code in the <head> of your html"
 
 */
